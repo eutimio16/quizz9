c842b6e1e4268f53af2f1c73a9a1a7b92f90668e
eutimio16 <eutimio@live.com.mx>
2015-04-16 10:57:37 -0500

    qq
    kk

--- q1.py
+++ q1.py
@@ -1,14 +1,14 @@
 __author__ = 'Eutimio'
 import math
-def distancia(x1, x2, y1, y2):
-    a=x1+x2
-    b=y1+y2
+def distancia(x1, y1, x2, y2):
+    a=x1+y2
+    b=y1+x2
     c=a*a + b*b
     c1=math.sqrt(c)
     return c1
 x1= int(input("enter size for x1"))
-x2= int(input("enter size for x2"))
 y1= int(input("enter size for y1"))
+x2= int(input("enter size for x2"))
 y2= int(input("enter size for y2"))
 d= distancia(x1, x2, y1, y2)
 print(d)
